557915162b3bc27244a33b9c7fd5af2b13c76598
funzeye <funzeye@gmail.com>
2014-12-22 15:52:44 +0000

    Initial Commit

---
+++ Phaser Project Template - TypeScript/app.ts
@@ -0,0 +1,28 @@
+﻿/// <reference path="node_modules\phaser\typescript\phaser.d.ts"/>
+
+class SimpleGame {
+
+    constructor() {
+        this.game = new Phaser.Game(800, 600, Phaser.AUTO, 'content', { preload: this.preload, create: this.create });
+    }
+
+    game: Phaser.Game;
+
+    preload() {
+        this.game.load.image('logo', 'img/phaser2.png');
+    }
+
+    create() {
+        var logo = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'logo');
+        logo.anchor.setTo(0.5, 0.5);
+        logo.scale.setTo(0.2, 0.2);
+        this.game.add.tween(logo.scale).to({ x: 1, y: 1 }, 2000, Phaser.Easing.Bounce.Out, true);
+    }
+
+}
+
+window.onload = () => {
+
+    var game = new SimpleGame();
+
+};
